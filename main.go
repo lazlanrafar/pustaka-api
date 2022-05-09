333c58cf148484c7500890b3da9883aab6b7f3a5
lazlanrafar <lazlanrafar@gmail.com>
2022-05-09 09:40:54 +0700

    path variabel

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main(){
 
 
 	router.GET("/", rootHandler)
+	router.GET("/path-variabel/:id", pathVariabelHandler)
 
 	router.Run(":8080")
 }	
@@ -19,5 +20,12 @@ func rootHandler(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"name": "Azlan Rafar",
 		"bio": "Programmer",
+	})
+}
+
+func pathVariabelHandler(c *gin.Context){
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
 	})
 }
